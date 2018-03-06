1b34fd9c891dea4d70904dbaca8d3bfb0856b819
Farkor123 <a.sadowski1@onet.eu>
2018-03-06 16:46:58 +0100

    Update Decode.py

--- Exercise 1/Decode.py
+++ Exercise 1/Decode.py
@@ -5,18 +5,35 @@ H = np.matrix([[0,1,1,1,1,0,0,1,1,0,0,0],[1,0,1,1,0,0,1,1,0,1,0,0],[0,1,1,0,1,1,
 
 def findErrorPosition(x, y):
     for i in range(0, 12):
-        if (y==x[:,i]).all(0):
-            return i
-def decode():
-    a = str(sys.argv[1])
-    T = np.matrix([[int(a[1])],[int(a[1])],[int(a[2])],[int(a[3])],[int(a[4])],[int(a[5])],[int(a[6])],[int(a[7])],[int(a[8])],[int(a[9])],[int(a[10])],[int(a[11])]])
-    print(T.getT())
-    G = (H*T)%2
-    i = findErrorPosition(H, G)
-    if T[i]==0:
-        T[i] = 1
-    else:
-        T[i] = 0
-    return T
+        for j in range(0,4):
+            if x[j,i] != y[j,0]:
+                break;
+            else:
+                if j == 3:
+                    return i
+    return -1
 
-print(decode().getT())
+def decode():
+    f = open("./file.txt", "r")
+    a = f.read()
+    f.close()
+    print(a)
+    T = np.matrix([[int(a[0])],[int(a[1])],[int(a[2])],[int(a[3])],[int(a[4])],[int(a[5])],[int(a[6])],[int(a[7])],[int(a[8])],[int(a[9])],[int(a[10])],[int(a[11])]])
+    G = (H*T)%2
+    print(H)
+    print(G)
+    i = findErrorPosition(H, G)
+    print(i)
+    if i >= 0 and i <= 11:
+        if T[i]==0:
+            T[i] = 1
+            b = a[0:i]+"1"+a[i+1:12]
+        else:
+            T[i] = 0
+            b = a[0:i]+"0"+a[i+1:12]
+        return b
+    else:
+        return a
+
+
+print(decode())
